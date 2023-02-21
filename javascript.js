9145df368c6e7fe199b1593df4e5ab91db2564a7
Wayne <opokupo@tutanota.com>
2023-02-21 15:39:51 +0800

    Create playRound function which returns result as string

--- javascript.js
+++ javascript.js
@@ -1,11 +1,42 @@
-const choice = ['Rock', 'Paper', 'Scissors'];
+const choice = ['rock', 'paper', 'scissors'];
 
 function getComputerChoice() {
     return choice[ Math.floor( Math.random() * 3 ) ];
 }
 
+function playRound( playerSelection, computerSelection ){
+    playerSelection = playerSelection.toLowerCase();
+
+    if (playerSelection === computerSelection) {
+        return "Draw!";
+    } else if (playerSelection === 'rock') {
+        if (computerSelection === 'paper'){
+            return "You Lose! Paper beats Rock";
+        } else {
+            return "You Win! Rock beats Scissors";
+        }
+    } else if (playerSelection === 'paper') {
+        if (computerSelection === 'scissors'){
+            return "You Lose! Scissors beats Paper";
+        } else {
+            return "You Win! Paper beats Rock";
+        }
+    } else {
+        if (computerSelection === 'rock'){
+            return "You Lose! Rock beats Scissors";
+        } else {
+            return "You Win! Scissors beats Paper";
+        }
+    }
+}
+
+
 function main(){
-    console.log( getComputerChoice() );
+    const playerSelection = "rock";
+    const computerSelection = getComputerChoice();
+    console.log( playerSelection );
+    console.log( computerSelection );
+    console.log(playRound(playerSelection, computerSelection));
 }
 
 main();

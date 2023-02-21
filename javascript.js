a25de3f68baecba72ecd7be33d1d3c9d6f6d6d32
Wayne <opokupo@tutanota.com>
2023-02-21 16:07:13 +0800

    Convert the return value of playRound from a string to -1,0,1

--- javascript.js
+++ javascript.js
@@ -8,35 +8,53 @@ function playRound( playerSelection, computerSelection ){
     playerSelection = playerSelection.toLowerCase();
 
     if (playerSelection === computerSelection) {
-        return "Draw!";
+        console.log( "Draw!" );
+        return 0;
     } else if (playerSelection === 'rock') {
         if (computerSelection === 'paper'){
-            return "You Lose! Paper beats Rock";
+            console.log( "You Lose! Paper beats Rock" );
+            return -1;
         } else {
-            return "You Win! Rock beats Scissors";
+            console.log( "You Win! Rock beats Scissors" );
+            return 1;
         }
     } else if (playerSelection === 'paper') {
         if (computerSelection === 'scissors'){
-            return "You Lose! Scissors beats Paper";
+            console.log( "You Lose! Scissors beats Paper" );
+            return -1;
         } else {
-            return "You Win! Paper beats Rock";
+            console.log( "You Win! Paper beats Rock" );
+            return 1;
         }
     } else {
         if (computerSelection === 'rock'){
-            return "You Lose! Rock beats Scissors";
+            console.log( "You Lose! Rock beats Scissors" );
+            return -1;
         } else {
-            return "You Win! Scissors beats Paper";
+            console.log( "You Win! Scissors beats Paper" );
+            return 1;
         }
     }
 }
 
 
-function main(){
-    const playerSelection = "rock";
-    const computerSelection = getComputerChoice();
-    console.log( playerSelection );
-    console.log( computerSelection );
-    console.log(playRound(playerSelection, computerSelection));
+// function main(){
+//     const playerSelection = "rock";
+//     const computerSelection = getComputerChoice();
+//     console.log( playerSelection );
+//     console.log( computerSelection );
+//     console.log(playRound(playerSelection, computerSelection));
+// }
+
+// main();
+
+function game() {
+    for ( let i = 0; i < 5; i++ ){
+        let playerSelection = prompt("Rock Paper Scissors?");
+        console.log( playRound( playerSelection, getComputerChoice() ) );
+    }
+
+
 }
 
-main();
+game();

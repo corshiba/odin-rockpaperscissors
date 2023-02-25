7b475b97a73be850b758b4caa26bf3979726e34b
Wayne <121597433+corshiba@users.noreply.github.com>
2023-02-25 16:56:36 +0800

    Display result per round to the resultDiv

--- javascript.js
+++ javascript.js
@@ -9,31 +9,33 @@ function getComputerChoice() {
 function playRound( playerSelection, computerSelection ){
     playerSelection = playerSelection.toLowerCase();
 
+    const resultDiv = document.querySelector(".result");
+
     if (playerSelection === computerSelection) {
-        console.log( "Draw!" );
+        resultDiv.textContent = "Draw!";
         return 0;
     } else if (playerSelection === 'rock') {
         if (computerSelection === 'paper'){
-            console.log( "You Lose! Paper beats Rock" );
+            resultDiv.textContent = "You Lose! Paper beats Rock";
             return -1;
         } else {
-            console.log( "You Win! Rock beats Scissors" );
+            resultDiv.textContent = "You Win! Rock beats Scissors";
             return 1;
         }
     } else if (playerSelection === 'paper') {
         if (computerSelection === 'scissors'){
-            console.log( "You Lose! Scissors beats Paper" );
+            resultDiv.textContent = "You Lose! Scissors beats Paper";
             return -1;
         } else {
-            console.log( "You Win! Paper beats Rock" );
+            resultDiv.textContent = "You Win! Paper beats Rock";
             return 1;
         }
     } else {
         if (computerSelection === 'rock'){
-            console.log( "You Lose! Rock beats Scissors" );
+            resultDiv.textContent = "You Lose! Rock beats Scissors";
             return -1;
         } else {
-            console.log( "You Win! Scissors beats Paper" );
+            resultDiv.textContent = "You Win! Scissors beats Paper";
             return 1;
         }
     }
@@ -42,7 +44,6 @@ function playRound( playerSelection, computerSelection ){
 const btns = document.querySelectorAll('button');
 
 btns.forEach( (btn) => {
-    console.log(btn.classList.value);
     btn.addEventListener( 'click', () => {
         playRound( btn.classList.value, getComputerChoice() );
     } );

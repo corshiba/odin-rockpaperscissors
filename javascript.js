47ca46b33978f6235ae96c7a7c1c4d9bae683244
Wayne <121597433+corshiba@users.noreply.github.com>
2023-02-25 17:06:44 +0800

    Add winner anouncement

--- javascript.js
+++ javascript.js
@@ -1,6 +1,7 @@
 const choice = ['rock', 'paper', 'scissors'];
 
-let score = 0;
+let playerScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
     return choice[ Math.floor( Math.random() * 3 ) ];
@@ -13,31 +14,36 @@ function playRound( playerSelection, computerSelection ){
 
     if (playerSelection === computerSelection) {
         resultDiv.textContent = "Draw!";
-        return 0;
     } else if (playerSelection === 'rock') {
         if (computerSelection === 'paper'){
             resultDiv.textContent = "You Lose! Paper beats Rock";
-            return -1;
+            computerScore++;
         } else {
             resultDiv.textContent = "You Win! Rock beats Scissors";
-            return 1;
+            playerScore++;
         }
     } else if (playerSelection === 'paper') {
         if (computerSelection === 'scissors'){
             resultDiv.textContent = "You Lose! Scissors beats Paper";
-            return -1;
+            computerScore++;
         } else {
             resultDiv.textContent = "You Win! Paper beats Rock";
-            return 1;
+            playerScore++;
         }
     } else {
         if (computerSelection === 'rock'){
             resultDiv.textContent = "You Lose! Rock beats Scissors";
-            return -1;
+            computerScore++;
         } else {
             resultDiv.textContent = "You Win! Scissors beats Paper";
-            return 1;
+            playerScore++;
         }
+    }
+
+    if ( playerScore > 4 ) {
+        resultDiv.textContent += " Player wins!";
+    } else if ( computerScore > 4 ) {
+        resultDiv.textContent += " Computer wins!";
     }
 }
 
